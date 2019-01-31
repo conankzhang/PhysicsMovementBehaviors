fbd5dc44949a2c1bee422c17bf78b55e32c86dcd
conankzhang <conankzhang@gmail.com>
2019-01-31 15:03:13 -0700

    Flocking: Tweak behavior

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -135,12 +135,11 @@ void ofApp::HandleNewBehavior(EBehavior DesiredBehavior)
 	case EBehavior::FLOCK:
 		WanderFlock = new CFlock(1, WanderBehaviors, ofColor::lightGray);
 		WanderBehaviors.push_back(SWeightedBehavior(new cwander_steering(), 1));
-		WanderBehaviors.push_back(SWeightedBehavior(new CDynamicLookWhereYouAreGoing(), 1));
 
 		Flock = new CFlock(15, FlockBehaviors, ofColor::black);
-		FlockBehaviors.push_back(SWeightedBehavior(new cseek_steering(WanderFlock->GetCenterOfMass()), 0.2f));
-		FlockBehaviors.push_back(SWeightedBehavior(new CDynamicSeparation(Flock->GetBoids()), 1.0f));
-		FlockBehaviors.push_back(SWeightedBehavior(new cseek_steering(Flock->GetCenterOfMass()), 0.5f));
+		FlockBehaviors.push_back(SWeightedBehavior(new cseek_steering(WanderFlock->GetCenterOfMass()), 0.5f));
+		FlockBehaviors.push_back(SWeightedBehavior(new CDynamicSeparation(Flock->GetBoids()), 2.0f));
+		FlockBehaviors.push_back(SWeightedBehavior(new cseek_steering(Flock->GetCenterOfMass()), 0.3f));
 		FlockBehaviors.push_back(SWeightedBehavior(new CDynamicLookWhereYouAreGoing(), 1));
 		break;
 	}
